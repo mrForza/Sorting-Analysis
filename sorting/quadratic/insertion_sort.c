6172b4321bb9a52ea5561c86040946f92b140387
Roman Gromov <gromov1921@bk.ru>
2023-12-05 22:00:16 +0300

    add: quadratic insertion_sort and swap function in utilc.c

---
+++ sorting/quadratic/insertion_sort.c
@@ -0,0 +1,11 @@
+#include "../../src/utils.c"
+
+void insertion_sort(int* array, unsigned int size) {
+    for (int i = 1; i < size; ++i) {
+        int k = i;
+        while (k > 0 && array[k - 1] > array[k]) {
+            swap(&array[k - 1], &array[k]);
+            --k;
+        }
+    }
+}
