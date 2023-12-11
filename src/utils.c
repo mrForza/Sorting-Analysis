3e121bbc258e9257ac5196062d71e326aa5e66f3
Roman Gromov <gromov1921@bk.ru>
2023-12-11 20:05:01 +0300

    add measure_time function

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,22 @@
+#include <sys/time.h>
+#include "windows.h"
+
+#define MULTIPLIER 1000000
+
 void swap(int* x, int* y) {
     int temp = *x;
     *x = *y;
     *y = temp;
+}
+
+unsigned long measure_time(void (*sorting_algorithm) (int* array, unsigned int size), int* array, unsigned int size) {
+    struct timeval start_time, finish_time;
+    gettimeofday(&start_time, NULL);
+    sorting_algorithm(array, size);
+    gettimeofday(&finish_time, NULL);
+
+    unsigned long start_time_in_microseconds = MULTIPLIER * start_time.tv_sec + start_time.tv_usec;
+    unsigned long finish_time_in_microseconds = MULTIPLIER * finish_time.tv_sec + finish_time.tv_usec;
+
+    return finish_time_in_microseconds - start_time_in_microseconds;
 }
